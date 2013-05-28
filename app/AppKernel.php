8a7e539a332b647dcec318aa28472888f90abd58
adrian <miguiadrian@gmail.com>
2013-05-29 00:17:55 +0200

    martes noche

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new ecloud\UsuarioBundle\UsuarioBundle(),
+            new eCloud\AdminBundle\AdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
